91f8973340f0de6c4c2a97bfd0acada2541ea2cc
saripudin15 <sarippudin1511@gmail.com>
2020-08-08 01:41:50 +0700

    products backend

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const app = express()
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/react-shopping-cart-db",{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+// mongoose.connect("mongodb://127.0.0.1:27017/react-shopping-cart-db",{
+//         useCreateIndex:true,
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true}).then(()=> {
+// console.log('Database Successfully Connected')},error =>{
+// console.log(error)});
+
+// const db = mongoose.connect("mongodb://localhost/react-shopping-cart-db", 
+//     {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     }).then(() => {
+//     console.log("Connected To Mongo Db DataBase");
+//     }).catch((err) => {
+//     console.log("DataBase Connection Error " + err);
+//     });
+
+
+const Product = mongoose.model(
+    "products",
+    new mongoose.Schema({
+        _id:{type: String, default: shortid.generate},
+        title: String,
+        description: String,
+        image: String,
+        price: Number,
+        availableSizes: [String],
+    })
+);
+
+app.get("/api/products",async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+});
+
+app.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.send(savedProduct);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deletedProduct);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("serve at ..."));
